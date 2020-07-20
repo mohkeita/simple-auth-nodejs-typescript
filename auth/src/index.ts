b9957f7683e80733b592d54c808d9625b4ed18ef
mohkeita <mohkeita13@gmail.com>
2020-07-20 20:49:13 +0200

    implement middlewares and errors handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
 import { signOutRouter } from "./routes/signout";
 import { signUpRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 
 const app = express();
@@ -14,6 +17,12 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+app.all('*', async (req, res) => {
+   throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 
 app.listen(3000, () => {

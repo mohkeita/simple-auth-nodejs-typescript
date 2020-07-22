af3569e4b62521e30b2d7e26029f3a8235231153
mohkeita <mohkeita13@gmail.com>
2020-07-22 16:58:59 +0200

    implement user model, bad request error and hash password

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
@@ -24,7 +25,48 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
+// local
 
-app.listen(3000, () => {
-   console.log('Listening on port 3000!');
-});
+const start = async () => {
+   try {
+      await mongoose.connect('mongodb://localhost:27017/auth', {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+         useCreateIndex: true
+      });
+      console.log('Connected to MongoDb');
+   } catch (err) {
+      console.error(err);
+   }
+
+   app.listen(3000, () => {
+      console.log('Listening on port 3000!');
+   });
+};
+
+
+// kubernetes
+/*
+const start = async () => {
+   try {
+      await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+         useCreateIndex: true
+      });
+      console.log('Connected to MongoDb');
+   } catch (err) {
+      console.error(err);
+   }
+
+   app.listen(3000, () => {
+      console.log('Listening on port 3000!');
+   });
+};
+
+ */
+
+start();
+
+
+
